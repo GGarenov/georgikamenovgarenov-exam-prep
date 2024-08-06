d40d68eadeca24af2762bf2f91caea1370308a58
Georgi <g_garenov@outlook.com>
2024-08-06 17:02:33 +0300

    The last test successfully passed!

--- library/tests/ui/header.test.js
+++ library/tests/ui/header.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
-test.only("Check header", async ({ page }) => {
+test("Check header", async ({ page }) => {
   await page.goto("http://localhost:8080"); // you can replace this URL with any page that includes the header
   const homeLink = await page.$('a[href="/"]');
   const text = await homeLink.textContent();
